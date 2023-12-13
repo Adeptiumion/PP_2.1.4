f724a7277788a91717775d711b60ee66c5febfce
Adept <adeptiumion@gmail.com>
2023-12-13 18:55:44 +0700

    createAllTypesOfDepedency

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    // Связывание через сканирование(поиск) бина по типу.
+    @Autowired
+    private Deth8 deth8;
+
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+
+        return ", смерть Кощея на игле :( " + deth8.toString();
     }
 }
